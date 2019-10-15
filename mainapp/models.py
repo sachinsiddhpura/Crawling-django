ea80e0341e59d1f3a56591452af90953ab8d45f0
Dhaval Savalia <dhaval.savalia6@gmail.com>
2019-10-15 19:31:57 +0530

    better python3.7, models and unittest

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,5 +5,22 @@ class MainProject(models.Model):
     PROJECT_NAME = models.CharField(max_length=50)
     HOMEPAGE = models.URLField(max_length=100)
 
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __unicode__(self):
+        return u'%s' % self.pk
+
     def __str__(self):
         return self.PROJECT_NAME
+
+    def get_absolute_url(self):
+        return reverse('mainapp_mainproject_detail', args=(self.pk,))
+
+
+    def get_update_url(self):
+        return reverse('mainapp_mainproject_update', args=(self.pk,))
